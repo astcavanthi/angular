f31103b62876ddb316b5d74dfd1ee904db8189dd
bitsolutions4u <bitsolutions4u@gmail.com>
2023-05-02 22:28:51 +0530

    environments settings change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://172.31.6.49:8000',
+  apiUrl: 'http://172.31.7.244:8000',
   oauthkey: "qCB4MNBGShxQ39DWDvZGG36ftCNa63NLJga4cVHH9gtxN9ddeh",
 
 };
